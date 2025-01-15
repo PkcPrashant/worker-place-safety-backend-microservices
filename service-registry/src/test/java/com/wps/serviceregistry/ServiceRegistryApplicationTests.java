25ccd4a254cf9433c976587f22bc7e1723e6f10a
Prashant Choudhary <pc22230@gmail.com>
2025-01-16 00:56:35 +0530

    microservice setup - eureka server and api gateway

---
+++ service-registry/src/test/java/com/wps/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wps.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
